b1f9b3e92133ac28ef637d40345a9bce315a761e
Joel Alejandro Jimenez Tipan <alejandro@MacBook-Pro-de-Joel.local>
2026-04-28 21:11:54 -0500

    Cuarto Commit

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "suma.h"
 #include "resta.h"
 #include "multiplicacion.h"
+#include "division.h"
 int main()
 {
     int a,b;
@@ -12,6 +13,7 @@ int main()
     printf("la suma es: %d\n", suma(a,b));
     printf("la resta es: %d\n", resta(a,b));
     printf("la multiplicacion es: %d\n", multiplicacion(a,b));
+    printf("la division es: %d\n", division(a,b));
 
     return 0;
 }
