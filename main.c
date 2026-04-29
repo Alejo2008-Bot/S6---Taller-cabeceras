6a11485c7381aba08ffb266a2a191cfaaa8c111b
Joel Alejandro Jimenez Tipan <alejandro@MacBook-Pro-de-Joel.local>
2026-04-28 20:50:31 -0500

    SegundoCommit

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "suma.h"
+#include "resta.h"
 int main()
 {
     int a,b;
@@ -6,4 +8,8 @@ int main()
     scanf("%d", &a);
     printf("ingrese el 2do numero: ");
     scanf("%d", &b);
+    printf("la suma es: %d\n", suma(a,b));
+    printf("la resta es: %d\n", resta(a,b));
+
+    return 0;
 }
